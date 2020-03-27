29dabb00fa33f7b92d67bb72abc273807fcdf514
PaulHol <>
2020-03-28 00:30:07 +0100

    Update index.php

--- index.php
+++ index.php
@@ -5,14 +5,14 @@ echo "PHP Check: OK <br>";
 
 
 include("dbconfig.php");
-/* Attempt MySQL server connection. Assuming you are running MySQL
-server with default setting (user 'root' with no password) */
+/* 
+Attempt MySQL server connection.
+*/
 $link = mysqli_connect(DBHOST, DBUSER, DBPWD, DBNAME);
 
 // Check connection
-if($link === false){
+if ($link === false) {
     die("ERROR: Could not connect. " . mysqli_connect_error());
-}else{
+} else {
     echo "DB Check: OK";
 }
-
