aede932235cca8c1f93ff2fe0d467044aabaa584
PaulHol <>
2020-03-28 00:26:57 +0100

    Update gen.php

--- gen.php
+++ gen.php
@@ -1,1 +1,29 @@
+<?php
+//set the header type to img
+
+
+//get & check the _GET params
+
+
+//get & check the banner template from db
+
+
+//get & check & check compatibility the background_image
+
+
+//get the associated textlines and the logo data
+//      replace the $xyz parts with the data
+
+
+//create the image, load the colores, load the fonts
+
+
+//loop over all textlines and place them in the image
+//      
+
+
+//place the logo on the image
+
+
+//done return the image
 
