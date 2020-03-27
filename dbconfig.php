84e164c801b8b0dee96d01d4ee80152cc5e68c22
PaulHol <>
2020-03-27 19:33:27 +0100

    .

---
+++ dbconfig.php
@@ -0,0 +1,5 @@
+<?php
+define('DBUSER','username');
+define('DBPWD','password');
+define('DBHOST','localhost');
+define('DBNAME','cptgen');
--- index.php
+++ index.php
@@ -1,16 +1,17 @@
 <?php
 /*
  */
-echo "PHP Check: OK";
+echo "PHP Check: OK <br>";
 
+
+include("dbconfig.php");
 /* Attempt MySQL server connection. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
-$link = mysqli_connect("localhost", "cpt", "123456789", "cptgen");
+$link = mysqli_connect(DBHOST, DBUSER, DBPWD, DBNAME);
 
 // Check connection
 if($link === false){
     die("ERROR: Could not connect. " . mysqli_connect_error());
-    echo "DB Check: NOT OK";
 }else{
     echo "DB Check: OK";
 }
