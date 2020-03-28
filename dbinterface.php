ab87e23baca8efb06e050f3a83b83bd333f0cb3c
PaulHol <>
2020-03-28 11:02:34 +0100

    Update dbinterface.php

--- dbinterface.php
+++ dbinterface.php
@@ -11,7 +11,59 @@ function connect()
     return $link;
 }
 
-function getBackgoundImageAttributes(){
-
+function getBackgoundImageContent($background_image_id)
+{
 }
 
+function getBackgoundImageAttributes($background_image_id)
+{
+    $background_image_id = mysqli_real_escape_string(connect(), $background_image_id);
+    $res = mysqli_query(connect(), "SELECT regional_group, station, airport FROM background_image WHERE id LIKE " . $background_image_id);
+
+    if (mysqli_num_rows($res) > 0)
+        return mysqli_fetch_array($res);
+    else
+        return false;
+}
+
+function getCompatibleTemplateIds($background_image_id)
+{
+    $background_image_id = mysqli_real_escape_string(connect(), $background_image_id);
+    $res = mysqli_query(connect(), "SELECT id_template FROM template_background WHERE id_background LIKE " . $background_image_id);
+    if (mysqli_num_rows($res) > 0)
+        return mysqli_fetch_all($res);
+    else
+        return false;
+}
+
+function getTemplate($background_image_id, $template_id)
+{
+    $background_image_id = mysqli_real_escape_string(connect(), $background_image_id);
+    $template_id = mysqli_real_escape_string(connect(), $template_id);
+    $res = mysqli_query(connect(), "SELECT template.rectangle_id_color, template.logo_x, template.logo_y, template.logo_ressource 
+                                    FROM template JOIN template_background ON template.id = template_background.id_template 
+                                    WHERE template_background.id_background LIKE " . $background_image_id);
+    if (mysqli_num_rows($res) > 0)
+        return mysqli_fetch_array($res);
+    else
+        return false;
+}
+
+
+function getAllColores()
+{
+    $res = mysqli_query(connect(), "SELECT id, R, G, B FROM colores");
+    if (mysqli_num_rows($res) > 0)
+        return mysqli_fetch_all($res);
+    else
+        return false;
+}
+
+function getAllFonts()
+{
+    $res = mysqli_query(connect(), "SELECT id, ressource FROM fonts");
+    if (mysqli_num_rows($res) > 0)
+        return mysqli_fetch_all($res);
+    else
+        return false;
+}
