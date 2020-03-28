ce057bb66243faa3ce98a379597a619679ae099a
paulhol <31701319+paulhollmann@users.noreply.github.com>
2020-03-28 22:50:28 +0100

    Update dbinterface.php

--- dbinterface.php
+++ dbinterface.php
@@ -10,11 +10,19 @@ function connect()
         $link = mysqli_connect(DBHOST, DBUSER, DBPWD, DBNAME);
     return $link;
 }
-function query($qstring)
+function query_array($qstring)
 {
     $res = mysqli_query(connect(), $qstring);
     if (mysqli_num_rows($res) > 0)
         return mysqli_fetch_array($res);
+    else
+        return false;
+}
+function query_all($qstring)
+{
+    $res = mysqli_query(connect(), $qstring);
+    if (mysqli_num_rows($res) > 0)
+        return mysqli_fetch_all($res);
     else
         return false;
 }
@@ -30,20 +38,20 @@ function getBackgoundImageContent($background_image_id)
 function getBackgoundImageAttributes($background_image_id)
 {
     $background_image_id = esc_str($background_image_id);
-    return query("SELECT id,regional_group, station, airport FROM background_image WHERE id LIKE " . $background_image_id);
+    return query_array("SELECT id,regional_group, station, airport FROM background_image WHERE id LIKE " . $background_image_id);
 }
 
 function getCompatibleTemplateIds($background_image_id)
 {
     $background_image_id = esc_str($background_image_id);
-    return query("SELECT id_template FROM template_background WHERE id_background LIKE " . $background_image_id);
+    return query_all("SELECT id_template FROM template_background WHERE id_background LIKE " . $background_image_id);
 }
 
 function getTemplate($background_image_id, $template_id)
 {
     $background_image_id = esc_str($background_image_id);
     $template_id = esc_str($template_id);
-    return query("SELECT template.rectangle_id_color, template.logo_x, template.logo_y, template.logo_ressource 
+    return query_array("SELECT DESTINCT template.rectangle_id_color, template.logo_x, template.logo_y, template.logo_ressource 
                     FROM template JOIN template_background ON template.id = template_background.id_template 
                     WHERE template_background.id_background LIKE " . $background_image_id .
         " AND template.id LIKE " . $template_id);
@@ -55,17 +63,17 @@ function getTemplate($background_image_id, $template_id)
 function getTextlines($template_id)
 {
     $template_id = mysqli_real_escape_string(connect(), $template_id);
-    return query("SELECT id, fontsize, rotation, position_x, position_y, id_color, id_font, text
+    return query_all("SELECT id, fontsize, rotation, position_x, position_y, id_color, id_font, text
                     FROM textline JOIN template_textlines ON textline.id = template_textlines.id_textline
                     WHERE template_textlines.id_template LIKE " . $template_id);
 }
 
 function getAllColores()
 {
-    return query("SELECT id, R, G, B FROM colores");
+    return query_all("SELECT id, R, G, B FROM colores");
 }
 
 function getAllFonts()
 {
-    return query("SELECT id, ressource FROM fonts");
+    return query_all("SELECT id, ressource FROM fonts");
 }
