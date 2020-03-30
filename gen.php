9c93a3d6d563cff0d779df05ed47bd7c63159e96
paulhol <31701319+paulhollmann@users.noreply.github.com>
2020-03-30 10:56:08 +0200

    Update gen.php

--- gen.php
+++ gen.php
@@ -40,23 +40,27 @@ foreach ($textlines as $textline) {
 $im = imagecreatetruecolor(1280, 720);
 $colordatas = getAllColors();
 if ($colordatas == false)
-    die("");
+    die("No colors avail");
 $colors = array();
 foreach ($colordatas as $colordata) {
+    var_dump($colordata);
+    echo "<br><br>";
     $colors[$colordata["id"]] = imagecolorallocate($im, $colordata["R"], $colordata["G"], $colordata["B"]);
 }
 var_dump($colors);
 echo "<br><br>";
 $fontdatas = getAllFonts();
 if ($fontdatas == false)
-    die("");
+    die("No fonts avail");
 $fonts = array();
-var_dump($fonts);
-echo "<br><br>";
+
 foreach ($fontdatas as $fontdata) {
+    var_dump($fontdata);
+    echo "<br><br>";
     $fonts[$fontdata["id"]] = realpath($fontdata["ressource"]);
 }
-
+var_dump($fonts);
+echo "<br><br>";
 //place the background and the colorbar
 //TODO
 imagefilledrectangle($im, 0, 720 - 155, 1280, 720, $colors[$template["rectangle_id_color"]]);
