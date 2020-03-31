8ef04f0caa619daf7354f37d69853b8188b975b3
paulhol <31701319+paulhollmann@users.noreply.github.com>
2020-03-31 12:04:24 +0200

    Update index.php

--- index.php
+++ index.php
@@ -19,28 +19,28 @@
     <div>
         <form id="input" action="">
             <label for="sc">stationcallsign</label>
-            <input type="text" name="sc" maxlength="10" placeholder="EDDF_W_TWR">
+            <input type="text" name="sc" maxlength="10" value="EDDF_W_TWR">
             <br>
             <label for="sd">stationname</label>
-            <input type="text" name="sn" maxlength="15" placeholder="Frankfurt Tower">
+            <input type="text" name="sn" maxlength="15" value="Frankfurt Tower">
             <br>
             <label for="tn">traineename</label>
-            <input type="text" name="tn" maxlength="15" placeholder="Max Mustertrainee">
+            <input type="text" name="tn" maxlength="15" value="Max Mustertrainee">
             <br>
             <label for="td">date</label>
-            <input type="text" name="dt" maxlength="10" placeholder="00.00.0000">
+            <input type="text" name="dt" maxlength="10" value="00.00.0000">
             <br>
             <label for="ts">timestart</label>
-            <input type="text" name="ts" maxlength="4" placeholder="1900">
+            <input type="text" name="ts" maxlength="4" value="1900">
             <br>
             <label for="te">timeend</label>
-            <input type="text" name="te" maxlength="4" placeholder="2000">
+            <input type="text" name="te" maxlength="4" value="2000">
             <br>
             <label for="bs">background_image_id</label>
-            <input type="text" name="bg" maxlength="1" placeholder="1">
+            <input type="text" name="bg" maxlength="1" value="1">
             <br>
             <label for="bs">template_id</label>
-            <input type="text" name="tp" maxlength="1" placeholder="1">
+            <input type="text" name="tp" maxlength="1" value="1">
             <br>
         </form>
         <br>
