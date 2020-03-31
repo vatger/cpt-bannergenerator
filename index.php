f064a208adb97ae61384c8803748c952f63beb42
paulhol <31701319+paulhollmann@users.noreply.github.com>
2020-03-31 11:55:35 +0200

    Update index.php

--- index.php
+++ index.php
@@ -12,8 +12,8 @@
     <script>
         function clicked_preview() {
             var data = $("#input").serialize();
-            $("#outputlink").html("//link-to-mainfolder/?" + data);
-            $("#preview_img").attr("src", "?" + data);
+            $("#outputlink").html("gen.php?" + data);
+            $("#preview_img").attr("src", "gen.php?" + data);
         }
     </script>
     <div>
