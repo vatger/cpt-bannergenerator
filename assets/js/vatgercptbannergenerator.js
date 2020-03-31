bf39c2c9d9b0caab1009e962a08a42da9dcd7c5e
paulhol <31701319+paulhollmann@users.noreply.github.com>
2020-03-31 23:26:52 +0200

    .

--- assets/js/vatgercptbannergenerator.js
+++ assets/js/vatgercptbannergenerator.js
@@ -1,10 +1,20 @@
 // VATGER CPT Bannergenerator by Paul Hollmann
 
+//load the predisplay
+$("#background_image_display").html;
+
+//set the button action
+$("#button").click(function (event) {
+    var data = $("#input").serialize();
+    $("#outputlink").html($(location).attr("origin") + "/gen.php?" + data);
+    $("#preview_img").attr("src", "gen.php?" + data);
+});
+
+//lazyload images
 $(document).ready(function () {
-    $('#button').click(function (event) {
-        var data = $("#input").serialize();
-        $("#outputlink").html($(location).attr("origin") + "/gen.php?" + data);
-        $("#preview_img").attr("src", "gen.php?" + data);
+    $("img[data-lazysrc]").each(function () {
+        //* set the img src from data-src
+        $(this).attr("src", $(this).attr("data-lazysrc"));
     });
 });
 
