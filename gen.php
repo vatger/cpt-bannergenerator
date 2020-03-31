2e96f5821ec369d1fea6361a1f13d6b16814272a
paulhol <31701319+paulhollmann@users.noreply.github.com>
2020-03-31 10:47:50 +0200

    Update gen.php

--- gen.php
+++ gen.php
@@ -66,7 +66,7 @@ $imagecontent = getBackgoundImageContent($background_image_id);
 if ($imagecontent == false)
     die("No background image data");
 var_dump($imagecontent["content"]);
-echo "<br><br>";
+echo "<br><br>br";
 var_dump(base64_decode($imagecontent["content"]));
 echo "<br><br>";
 $im_background = imagecreatefromstring(base64_decode($imagecontent["content"]));
