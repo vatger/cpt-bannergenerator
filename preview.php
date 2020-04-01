caa22ae3e3672ad1a9b801217fac7b1e7f55893c
paulhol <31701319+paulhollmann@users.noreply.github.com>
2020-04-01 12:18:49 +0200

    Update preview.php

--- preview.php
+++ preview.php
@@ -7,6 +7,8 @@ $filter_station = "TWR";
 $filter_rg = "EDFF";
 
 $im_db = getBackgoundImageViaFilter($number, $filter_rg, $filter_station, $filter_airport);
+
+/*
 if ($im_db != false) {
 ?>
     <img src='https://via.placeholder.com/140x100' class='img' data-imageid="<?php echo $im_db["id"]; ?>"></img>
@@ -16,3 +18,4 @@ if ($im_db != false) {
     <img src='https://via.placeholder.com/140x100' class='img' data-imageid="null"></img>
 <?php
 }
+*/
