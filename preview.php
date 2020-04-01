d2f4ed4b0c7b292304bcf731222d2e97ccc66e6a
paulhol <31701319+paulhollmann@users.noreply.github.com>
2020-04-01 13:53:25 +0200

    .

--- preview.php
+++ preview.php
@@ -1,24 +1,25 @@
 <?php
 require_once("dbinterface.php");
+if (true) {
+    $number = urldecode($_GET["number"]);
+    $filter_airport = "EDDF";
+    $filter_station = "TWR";
+    $filter_rg = "EDFF";
 
-$number = urldecode($_GET["number"]);
-$filter_airport = "EDDF";
-$filter_station = "TWR";
-$filter_rg = "EDFF";
-sleep(2);
-$im_db = getBackgoundImageViaFilter($number, $filter_rg, $filter_station, $filter_airport);
+    $im_db = getBackgoundImageViaFilter($number, $filter_rg, $filter_station, $filter_airport);
 
-$im = imagescale(imagecreatefromstring($im_db["content"]), 200);
-ob_start();
-imagepng($im);
-$stringdata = ob_get_contents();
-ob_end_clean();
-if ($im_db != false) {
+    $im = imagescale(imagecreatefromstring($im_db["content"]), 150);
+    ob_start();
+    imagepng($im);
+    $stringdata = ob_get_contents();
+    ob_end_clean();
+    if ($im_db != false) {
 ?>
-    <img src="data:image/png;base64,<?php echo base64_encode($stringdata); ?>" class='img' data-imageid="<?php echo $im_db["id"]; ?>"></img>
+        <img src="data:image/png;base64,<?php echo base64_encode($stringdata); ?>" class='img' data-imageid="<?php echo $im_db["id"]; ?>"></img>
+    <?php
+    } else {
+    ?>
+        <img src='https://via.placeholder.com/200x88' class='img' data-imageid="null"></img>
 <?php
-} else {
-?>
-    <img src='https://via.placeholder.com/140x100' class='img' data-imageid="null"></img>
-<?php
+    }
 }
