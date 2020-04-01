28e8f3cab72c75976b776a8028436958ed639399
paulhol <31701319+paulhollmann@users.noreply.github.com>
2020-04-01 22:30:21 +0200

    Update vatgercptbannergenerator.js

--- assets/js/vatgercptbannergenerator.js
+++ assets/js/vatgercptbannergenerator.js
@@ -1,50 +1,51 @@
 // VATGER CPT Bannergenerator by Paul Hollmann
 
 //load the predisplay
-$.get("preview.php", { rg: "", station: "", airport: "" }, function (data) {
-    var count = parseInt(data);
-    if (isNaN(count) || count == 0) {
-        $("#background_image_display").html = "<div class='row align-middle'><span class='badge badge-dark'>No images found</span></div>";
-    } else {
-        for (let i = 0; i * 6 < count; i++) {
-            var row = $("<div class='row mt-2'></div>");
-            row.appendTo("#background_image_display");
-            for (let j = 0; j < 6; j++) {
-                var col = $("<div class='col align-items-center align-middle'></div>");
-                getPreview(col, i, j);
-                row.append(col);
+function predisplay() {
+    $("#background_image_display").html = "LOADING";
+    $.get("preview.php", { rg: "", station: "", airport: "" }, function (data) {
+        var count = parseInt(data);
+        if (isNaN(count) || count == 0) {
+            $("#background_image_display").html = "<div class='row align-middle'><span class='badge badge-dark'>No images found</span></div>";
+        } else {
+            for (let i = 0; i * 6 < count; i++) {
+                var row = $("<div class='row mt-2'></div>");
+                row.appendTo("#background_image_display");
+                for (let j = 0; j < 6; j++) {
+                    var col = $("<div class='col align-items-center align-middle'></div>");
+                    getPreview(col, i, j);
+                    row.append(col);
+                }
             }
         }
-    }
-});
-function getPreview(col, i, j) {
-    col.html('<div class="spinner-border" role="status"><span class="sr-only">Loading...</span></div>');
-    $.get("preview.php", { number: (i * 6 + j) }, function (data) {
-        col.html(data);
-        addEvents(col);
     });
-}
-function addEvents(col) {
-    col.on("background_image_selected", function () {
-        col.children("img").removeClass("border");
-        col.children("img").removeClass("border-success");
-        col.children("img").removeClass("img-thumbnail");
-    });
-    var id = parseInt(col.children("img").attr("data-imageid"));
-    if (!isNaN(id) && id > 0) {
-        col.click(function () {
-            $(".col").each(function () {
-                $(this).trigger("background_image_selected");
-            })
-            col.children("img").addClass("img-thumbnail");
-            col.children("img").addClass("border");
-            col.children("img").addClass("border-success");
-            $("#form_bg").val(parseInt(col.children("img").attr("data-imageid")));
+    function getPreview(col, i, j) {
+        col.html('<div class="spinner-border" role="status"><span class="sr-only">Loading...</span></div>');
+        $.get("preview.php", { number: (i * 6 + j) }, function (data) {
+            col.html(data);
+            addEvents(col);
         });
-
+    }
+    function addEvents(col) {
+        col.on("background_image_selected", function () {
+            col.children("img").removeClass("border");
+            col.children("img").removeClass("border-success");
+            col.children("img").removeClass("img-thumbnail");
+        });
+        var id = parseInt(col.children("img").attr("data-imageid"));
+        if (!isNaN(id) && id > 0) {
+            col.click(function () {
+                $(".col").each(function () {
+                    $(this).trigger("background_image_selected");
+                })
+                col.children("img").addClass("img-thumbnail");
+                col.children("img").addClass("border");
+                col.children("img").addClass("border-success");
+                $("#form_bg").val(parseInt(col.children("img").attr("data-imageid")));
+            });
+        }
     }
 }
-
 
 //set the button action
 $("#button").click(function (event) {
