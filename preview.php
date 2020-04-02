57d517b954efef0a98a47dbffb321d2c1fa30b4c
paulhol <31701319+paulhollmann@users.noreply.github.com>
2020-04-02 11:21:00 +0200

    Update preview.php

--- preview.php
+++ preview.php
@@ -22,7 +22,7 @@ if (!isset($_GET["number"])) {
     ob_end_clean();
     if ($im_db != false) {
 ?>
-        <img src="data:image/png;base64,<?php echo base64_encode($stringdata); ?>" class="img img-fluid" data-imageid="<?php echo $im_db["id"]; ?>" data-toggle="tooltip" data-placement="left" data-html="true" title="<b>RG</b> <?php echo $im_db["regional_group"]; ?>  <b>Station</b> <?php echo $im_db["station"]; ?> <b>Airport</b> <?php echo $im_db["airport"]; ?>">
+        <img src="data:image/png;base64,<?php echo base64_encode($stringdata); ?>" class="img img-fluid" data-imageid="<?php echo $im_db["id"]; ?>" data-toggle="tooltip" data-placement="left" data-html="true" title="<b>RG</b> <?php echo $im_db["regional_group"]; ?> <br><b>Station</b> <?php echo $im_db["station"]; ?> <br><b>Airport</b> <?php echo $im_db["airport"]; ?>">
         </img>
     <?php
     } else {
