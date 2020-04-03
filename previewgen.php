d6c50123e086669f07f69c89ef1878c89e38c1aa
paulhol <31701319+paulhollmann@users.noreply.github.com>
2020-04-03 22:05:58 +0200

    Create previewgen.php

---
+++ previewgen.php
@@ -0,0 +1,55 @@
+<?php
+require_once("dbinterface.php");
+
+//get & check the _GET params
+$params_valid = true;
+$template_id = urldecode($_GET["tp"]);
+if(intval($template_id) == 0)
+    $params_valid = false;
+if (!$params_valid)
+    die("Wrong params");
+
+//get the associated textlines and the logo data
+//      replace the $xyz parts with the data
+$textlines = getTextlines($template_id);
+if ($textlines == false)
+    die("No textlines avail");
+
+//create the image, load the colores, load the fonts
+$im = imagecreatetruecolor(1280, 720);
+$colordatas = getAllColors();
+if ($colordatas == false)
+    die("No colors avail");
+$colors = array();
+imagecolorallocate($im, 100, 100, 100);
+foreach ($colordatas as $colordata) {
+    $colors[$colordata["id"]] = imagecolorallocate($im, intval($colordata["R"]), intval($colordata["G"]), intval($colordata["B"]));
+}
+$fontdatas = getAllFonts();
+if ($fontdatas == false)
+    die("No fonts avail");
+$fonts = array();
+foreach ($fontdatas as $fontdata) {
+    $fonts[$fontdata["id"]] = realpath($fontdata["ressource"]);
+}
+
+//the colorbar
+imagefilledrectangle($im, 0, 720 - 155, 1280, 720, $colors[$template["rectangle_id_color"]]);
+
+//loop over all textlines and place them in the image
+foreach ($textlines as $textline) {
+    ImageTTFText($im, intval($textline["fontsize"]),  intval($textline["rotation"]),  intval($textline["position_x"]),  intval($textline["position_y"]), $colors[$textline["id_color"]], $fonts[$textline["id_font"]], $textline["text"]);
+}
+
+//place the logo on the image
+$im_logo = imagecreatefrompng($template["logo_ressource"]);
+imagecopymerge($im, $im_logo, intval($template["logo_x"]), intval($template["logo_y"]), 0, 0, imagesx($im_logo), imagesy($im_logo), 100);
+
+//set the header type to img
+header("Content-type: image/png");
+
+//done return the image
+imagepng($im);
+imagedestroy($im);
+imagedestroy($im_background);
+imagedestroy($im_logo);
