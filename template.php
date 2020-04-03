b5e7c4250c14ed5e99caa67939e5632087d668a8
paulhol <31701319+paulhollmann@users.noreply.github.com>
2020-04-03 22:19:59 +0200

    Update template.php

--- template.php
+++ template.php
@@ -3,7 +3,7 @@ require_once("dbinterface.php");
 $filter_bg_id = urldecode($_GET["background_id"]);
 
 if (!isset($_GET["number"])) {
-    $count = getTeamplateCountViaFilter($filter_bg_id);
+    $count = getTemplateCountViaFilter($filter_bg_id);
     if ($count == false)
         $count = 0;
     else
@@ -12,7 +12,7 @@ if (!isset($_GET["number"])) {
 } else {
     $number = urldecode($_GET["number"]);
 
-    $template = getTeamplateViaFilter($number, $filter_bg_id);
+    $template = getTemplateViaFilter($number, $filter_bg_id);
 
     if ($template == false) {
         echo "<code><em>false</em></code>";
