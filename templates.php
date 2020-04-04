cd042f19b58972172466a18aace382f54c3cdf65
paulhol <31701319+paulhollmann@users.noreply.github.com>
2020-04-04 22:43:08 +0200

    Update templates.php

--- templates.php
+++ templates.php
@@ -13,7 +13,7 @@ if ($templates == false) {
     foreach ($templates as $template) {
         $id = $template["id_template"];
     ?>
-        <div class="col-auto">
+        <div class="col-auto mt-1 mb-1">
             <img alt="IMAGE" data-lazysrc="templategen.php?tp=<?php echo $id;?>" class="img-fluid mx-auto d-block" data-templateid="<?php echo $id ?>">
             </img>
         </div>
