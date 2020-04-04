0ad15e50448158d0fc1edf6c45a0ed6146f0e92b
paulhol <31701319+paulhollmann@users.noreply.github.com>
2020-04-04 21:41:24 +0200

    Update backgrounds.php

--- backgrounds.php
+++ backgrounds.php
@@ -13,6 +13,7 @@ if ($background_datas == false) {
 <?php
 }
 else{
+    var_dump($background_datas);
     foreach ($background_datas as $background_data){
         ?>
         <div class="col-auto">
