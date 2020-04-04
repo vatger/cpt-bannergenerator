fc9811d97a30aca2fca3e7baa802dd6d1dc235de
paulhol <31701319+paulhollmann@users.noreply.github.com>
2020-04-04 21:00:43 +0200

    Create backgrounds.php

---
+++ backgrounds.php
@@ -0,0 +1,23 @@
+<?php
+require_once("dbinterface.php");
+$filter_rg = urldecode($_GET["rg"]);
+$filter_station = urldecode($_GET["station"]);
+$filter_airport = urldecode($_GET["airport"]);
+
+$background_datas = getBackgoundImageDataViaFilter($filter_rg, $filter_station, $filter_airport);
+if ($background_datas == false) {
+?>
+    <div class="col-auto align-items-center align-middle">
+        <span class="badge badge-dark">No images found</span>
+    </div>
+<?php
+}
+else{
+    foreach ($background_datas as $background_data){
+        ?>
+        <div class="col-auto">
+            <img src="assets/img/150x66.png" class="img-fluid mx-auto d-block" data-imageid="NULL">
+        </div>
+        <?php
+    }
+}
